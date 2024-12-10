053726f2bd8473cae759b7810fc01fbb737e7463
20gg <245387976@qq.com>
2024-12-10 09:30:02 +0800

    Update [Lean's LEDE]HC5661-part2.sh

--- diy-part2/[Lean's LEDE]HC5661-part2.sh
+++ diy-part2/[Lean's LEDE]HC5661-part2.sh
@@ -20,7 +20,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 echo '修改机器名称'
-sed -i 's/OpenWrt/Gee_1s/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/STEAM/g' package/base-files/files/bin/config_generate
 
 target_inf() {
     #=========================================
